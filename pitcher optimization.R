bc07f0bf1ff07026639a03c4c4231a5c26002e8e
ct-analytics <chrismtster@gmail.com>
2015-05-26 23:13:57 -0400

    updated optimization and salary viz

--- pitcher optimization.R
+++ pitcher optimization.R
@@ -37,3 +37,48 @@ get.variables(ip)
 get.constraints(ip)
 
 pitchers$Player[as.logical(get.variables(ip))]
+pitchers$select <- as.factor(get.variables(ip))
+
+write.csv(pitchers,"pitchers.csv")
+
+library(ggplot2)
+library(scales)
+library(RColorBrewer)
+
+ggplot(data=pitchers,aes(x=GS,y=Salary,group=select),alpha=abs(select-.2)) +
+#   geom_point() +
+  geom_text(aes(label=Player,color=select),size=3) +
+  scale_y_continuous(labels = dollar) +
+  scale_color_discrete(guide=F)
+
+library(rCharts)
+
+pitchers$colors[pitchers$select==1] <- "#66CC99"
+pitchers$colors[pitchers$select==0] <- "#CC6666"
+
+h <- hPlot(Salary ~ GS, data = pitchers, 
+      type = c("scatter"), group = "Player", size = 3)
+# h$tooltip(formatter = "#! function() { return this.name + ', ' + this.x + ', ' + this.y; } !#")
+h$tooltip(headerFormat= '<b>{series.name}</b><br>',
+          pointFormat= 'Game Score: {point.x}<br>Salary: ${point.y:,.0f}')
+# var salary = '$' + point.y.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+h$chart(zoomType = "xy")
+h$plotOptions(scatter = list(marker = list(symbol = 'circle')))
+h$legend(align = 'right', verticalAlign = 'bottom', layout = 'vertical'
+          ,x= -30,y= -60,floating= TRUE,enabled=FALSE)
+# h$colors(pitchers$colors)
+h$yAxis(title=list(text="Salary"),labels=list(format='${value:,.0f}'))
+h$xAxis(title=list(text="Game Score"))
+h$title(text="Draft Kings Optimization")
+h
+  
+library(plyr)
+l <- dlply(select(pitchers,x=GS,y=Salary,color=colors),1,c)
+a <- rCharts::Highcharts$new()
+for(i in l) {
+  a$series(data = i, type = "scatter", name = pitchers$Player[match(i,l)])
+}
+a$xAxis(title = list(text = "Game Score"))
+a$yAxis(title = list(text = "Salary"))
+# a$legend(enabled = F)
+a
--- salary data.R
+++ salary data.R
@@ -14,14 +14,19 @@ dk.pitchers <- dk.20150521 %>%
   select(Player,Salary) %>%
   filter(!(is.na(Salary)))
 
-dk.batters <- dk.raw %>%
-  html_nodes("table") %>%
-  .[[5]] %>%
-  html_table(fill=TRUE) %>%
-  mutate(Player = str_replace(X2,"\\^\\d",""),
-         Salary = as.numeric(str_replace_all(X4,"\\$|,",""))) %>%
-  select(Player,Salary) %>%
-  filter(row_number() > 126)
 
+library(rCharts)
+h <- hPlot(Salary ~ Player, 
+           data = dk.pitchers %>%
+             arrange(desc(Salary)) %>%
+             slice(1:15), 
+           type = c("bar"))
+h$tooltip(headerFormat= '<b>{point.x}</b><br>',
+          pointFormat= 'Salary: ${point.y:,.0f}')
+h$yAxis(title=list(text="Salary"),labels=list(format='${value:,.0f}'))
 
-
+outf <- h$print()
+str(h)
+    output <- file("salary chart.txt")
+writeLines(h$print(),output)
+close(output)
